37da6e80fde54e5fef85e59fbed9beb11ca97632
Mustafa Bayar <45703322+mustafabayarr@users.noreply.github.com>
2020-09-15 00:31:12 +0300

    Add files via upload

---
+++ FindANumber.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package findanumber;
+
+/**
+ *
+ * @author Mustafa Bayar
+ */
+public class FindANumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //aranacak sayıyı bulma
+        int[] numbers = {1,2,3,4,5};
+        int searchFor = 44;
+        
+        for(int number: numbers){
+            if(number == searchFor){
+                System.out.println("sayı bulundu" + number);
+            }
+        }
+        System.out.println("sayi mevcut değildir.");
+    }
+}
