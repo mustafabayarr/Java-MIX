a0d4135d90fae6cb8a93f0d360633c52f9fb3476
Mustafa Bayar <45703322+mustafabayarr@users.noreply.github.com>
2020-09-15 00:13:25 +0300

    Add files via upload

---
+++ FriendNumbers.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package friendnumbers;
+
+/**
+ *
+ * @author Mustafa Bayar
+ */
+public class FriendNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number1 = 221;
+        int number2 = 284;
+        int sum1 = 0;
+        int sum2 = 0;
+        
+        for(int i = 1;i<number1;i++){
+            if(number1 % i == 0){
+                sum1 = sum1 + i;
+            }
+        }
+        for(int i = 1;i<number2;i++){
+            if(number2 % i == 0){
+                sum2 = sum2 + i;
+            }
+        }
+        
+        if(number1 == sum2 && number2 == sum1){
+            System.out.println(number1 + " ile " + number2 + " arkadaş sayılardır.");
+        }
+        else{
+            System.out.println(number1 + " ile " + number2 + " arkadaş sayılar değildir.");
+        }
+    }
+}
