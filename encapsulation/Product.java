bdd1cd2f5fd1a44d6c8d5b8042b2ed40c7e971e6
Mustafa Bayar <45703322+mustafabayarr@users.noreply.github.com>
2020-09-15 20:33:27 +0300

    Add files via upload

---
+++ encapsulation/Product.java
@@ -0,0 +1,21 @@
+package encapsulation;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mustafa Bayar
+ */
+public class Product {
+    
+    // attribute | field
+    int id;
+    String name;
+    String description;
+    double price;
+    int stockAmount;
+}
